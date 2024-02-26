df1517dd4f7dbe9c5fc3504aefed6a6f844e2989
Kapil Shrestha <kapilshrestha1001@gmail.com>
2024-02-26 17:41:06 +0545

    Add: Dist folder in vite config for build command

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,22 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "./",
-  
+  base: './',
+
+
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    // Set the base directory for resolving entry points and assets
+    outDir: './dist',
+    // Configure assets handling
+    assetsDir: 'assets', // Specifies the directory name for assets in the build output
+    assetsInlineLimit: 4096, // The maximum size of files to inline in the build output (in bytes)
+    cssCodeSplit: true, // Enable CSS code splitting
+    sourcemap: true, // Generate source maps for the build output
   },
 })
