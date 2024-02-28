beef5f1ab8a7fa97c70662c287014c141f62b4d3
Kapil Shrestha <kapilshrestha1001@gmail.com>
2024-02-28 11:21:34 +0545

    function rolled back to products search only

--- src/components/elements/SearchTest/SearchInput.tsx
+++ src/components/elements/SearchTest/SearchInput.tsx
@@ -1,41 +1,33 @@
 import { FC, useState } from "react";
 import MyLiveSearch from "./MyLiveSearchBox";
 import { products } from "@/containers/Products/ProductsArray";
-import { services } from "@/containers/Services/ServicesArray";
 
-const MyApp: FC = (): JSX.Element => {
-  const [results, setResults] = useState<(typeof products[number] | typeof services[number])[]>();
-  const [selectedItem, setSelectedItem] = useState<(typeof products[number] | typeof services[number])>();
+interface Props {}
+
+const MyApp: FC<Props> = (props): JSX.Element => {
+  const [results, setResults] = useState<typeof products[number][]>();
+  const [selectedProduct, setSelectedProduct] = useState<typeof products[number]>();
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const { value } = e.target;
     const trimmedValue = value.trim().toLowerCase();
-  
-    if (!trimmedValue) return setResults([]);
-  
-    const combinedArray = [...products, ...services];
-  
-    console.log("Combined array:", combinedArray); // Add this line for debugging
-  
-    const filteredResults = combinedArray.filter(item =>
-      item.cardTitle.toLowerCase().includes(trimmedValue)
-    );
-  
-    console.log("Filtered results:", filteredResults); // Add this line for debugging
-  
-    setResults(filteredResults);
-  };
 
-  
-  
+    if (!trimmedValue) return setResults([]);
+
+    const filteredValue = products.filter((product) =>
+      product.cardTitle.toLowerCase().includes(trimmedValue)
+    );
+
+    setResults(filteredValue);
+  };
 
   return (
     <MyLiveSearch
       results={results}
-      value={selectedItem?.cardTitle}
+      value={selectedProduct?.cardTitle}
       renderItem={(item) => <p>{item.cardTitle}</p>}
       onChange={handleChange}
-      onSelect={(item) => setSelectedItem(item)}
+      onSelect={(item) => setSelectedProduct(item)}
     />
   );
 };
